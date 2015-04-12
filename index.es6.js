d5635da633db7d0ca20e974321e64483da635353
Aaron Shafovaloff <aaronshaf@gmail.com>
2015-04-11 21:21:13 -0600

    WIP

--- index.es6.js
+++ index.es6.js
@@ -64,17 +64,8 @@ const PublishToggle = React.createClass({
           onBlur={this.handleBlur}
           {...this.props}
       />
-        <svg className="ReactPublishToggle__svg" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-          <defs>
-            <filter id="f2" x="0" y="0" width="200%" height="200%">
-              <feOffset result="offOut" in="SourceGraphic" dx="20" dy="20" />
-              <feFlood flood-color="#3D4574" flood-opacity="0.5" result="offsetColor"/>
-              <feGaussianBlur result="blurOut" in="offOut" stdDeviation="10" />
-              <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
-            </filter>
-          </defs>
-
-          <path filter="url(#f2)" className="ReactPublishToggle__cloud"
+        <svg className="ReactPublishToggle__svg" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32">
+          <path className="ReactPublishToggle__cloud"
             d="M26.287 12.893c.073-.406.125-.82.125-1.248 0-3.824-3.097-6.925-6.927-6.925-3.35 0-6.14 2.377-6.783 5.534-.583-.227-1.21-.362-1.873-.362-2.87 0-5.193 2.326-5.193 5.194 0 .203.037.397.06.595-2.075.96-3.522 3.05-3.522 5.487 0 3.05 2.245 6.06 5.194 6.06h14.71c4.31 0 7.795-3.49 7.795-7.792 0-2.75-1.434-5.157-3.588-6.543z"/>
           <path
              className="ReactPublishToggle__check"
--- index.js
+++ index.js
@@ -83,20 +83,8 @@ var PublishToggle = _React2['default'].createClass({
       }, this.props)),
       _React2['default'].createElement(
         'svg',
-        { className: 'ReactPublishToggle__svg', xmlns: 'http://www.w3.org/2000/svg', width: '32', height: '32', viewBox: '0 0 32 32' },
-        _React2['default'].createElement(
-          'defs',
-          null,
-          _React2['default'].createElement(
-            'filter',
-            { id: 'f2', x: '0', y: '0', width: '200%', height: '200%' },
-            _React2['default'].createElement('feOffset', { result: 'offOut', 'in': 'SourceGraphic', dx: '20', dy: '20' }),
-            _React2['default'].createElement('feFlood', { 'flood-color': '#3D4574', 'flood-opacity': '0.5', result: 'offsetColor' }),
-            _React2['default'].createElement('feGaussianBlur', { result: 'blurOut', 'in': 'offOut', stdDeviation: '10' }),
-            _React2['default'].createElement('feBlend', { 'in': 'SourceGraphic', in2: 'blurOut', mode: 'normal' })
-          )
-        ),
-        _React2['default'].createElement('path', { filter: 'url(#f2)', className: 'ReactPublishToggle__cloud',
+        { className: 'ReactPublishToggle__svg', xmlns: 'http://www.w3.org/2000/svg', width: '22', height: '22', viewBox: '0 0 32 32' },
+        _React2['default'].createElement('path', { className: 'ReactPublishToggle__cloud',
           d: 'M26.287 12.893c.073-.406.125-.82.125-1.248 0-3.824-3.097-6.925-6.927-6.925-3.35 0-6.14 2.377-6.783 5.534-.583-.227-1.21-.362-1.873-.362-2.87 0-5.193 2.326-5.193 5.194 0 .203.037.397.06.595-2.075.96-3.522 3.05-3.522 5.487 0 3.05 2.245 6.06 5.194 6.06h14.71c4.31 0 7.795-3.49 7.795-7.792 0-2.75-1.434-5.157-3.588-6.543z' }),
         _React2['default'].createElement('path', {
           className: 'ReactPublishToggle__check',
